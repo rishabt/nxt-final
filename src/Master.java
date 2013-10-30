038f501d4fb0257f48df9bf1573746daa2130216
aj0strow <alexander.ostrow@gmail.com>
2013-10-30 18:48:38 -0400

    bluetooth

--- src/Master.java
+++ src/Master.java
@@ -1,7 +1,40 @@
-public class Master {
+import java.io.*;
+import lejos.nxt.*;
+import lejos.nxt.comm.*;
+import javax.bluetooth.*;
+
+
+public class Master {	
 
 	public static void main(String[] args) {
+		Button.waitForAnyPress();
 		
+		LCD.drawString("Connecting...", 0, 0);
+		
+		BTConnection connection = Bluetooth.waitForConnection();
+
+		if (connection == null) {
+			LCD.clear();
+			LCD.drawString("ERROR: connection failed", 0, 1);
+			Button.waitForAnyPress();
+			System.exit(1);
+		}
+		
+		DataInputStream inStream = connection.openDataInputStream();
+		DataOutputStream outStream = connection.openDataOutputStream();
+		
+		try {
+			String message = inStream.readUTF();
+			LCD.drawString(message, 0, 2);
+		} catch (IOException e) {}
+		
+		try {
+			inStream.close();
+			outStream.close();
+			connection.close();
+		} catch (IOException e) {}
+		
+		Button.waitForAnyPress();
 	}
 
 }
--- src/Slave.java
+++ src/Slave.java
@@ -1,7 +1,46 @@
-public class Slave {
+import java.io.*;
+import lejos.nxt.*;
+import lejos.nxt.comm.*;
+import javax.bluetooth.*;
 
+public class Slave {
+	
 	public static void main(String[] args) {
+		Button.waitForAnyPress();
 		
+		LCD.drawString("Connecting...", 0, 0);
+		
+		RemoteDevice device = Bluetooth.getKnownDevice("master");
+		if (device == null) {
+			LCD.clear();
+			LCD.drawString("ERROR: device not found", 0, 1);
+			Button.waitForAnyPress();
+			System.exit(1);
+		}
+		
+		BTConnection connection = Bluetooth.connect(device);
+		if (connection == null) {
+			LCD.clear();
+			LCD.drawString("ERROR: connection failed", 0, 1);
+			Button.waitForAnyPress();
+			System.exit(1);
+		}
+		
+		DataInputStream inStream = connection.openDataInputStream();
+		DataOutputStream outStream = connection.openDataOutputStream();
+		
+		try {
+			outStream.writeUTF("hello!");
+			outStream.flush();
+		} catch (IOException e) {}
+		
+		try {
+			inStream.close();
+			outStream.close();
+			connection.close();
+		} catch (IOException e) {}
+		
+		Button.waitForAnyPress();
 	}
 
 }
